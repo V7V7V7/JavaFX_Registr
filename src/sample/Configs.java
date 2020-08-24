eb8f3228c0a73aacd386291c0047b1f1e4305259
V7V7V7 <ubiitsa@list.ru>
2020-08-24 13:58:04 +0300

    Finish

--- src/sample/Configs.java
+++ src/sample/Configs.java
@@ -4,6 +4,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "vadimi4";
+    protected String dbPass = "*****";
     protected String dbName = "vvv_tables";
 }
